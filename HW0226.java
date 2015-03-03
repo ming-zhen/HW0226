4080b3d5c17ecb074d91c7fa82e3c995e4f8d2d6
ming-zhen <pp6254585@gmail.com>
2015-03-04 00:10:22 +0800

    HW0226

---
+++ HW0226.java
@@ -0,0 +1,11 @@
+class HW0226 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("\n\n	 《回鄉偶書》");
+		System.out.println("	 作者：賀知章\n");
+		System.out.println("少小離家老大回，鄉音無改鬢毛衰。");
+		System.out.println("兒童相見不相識，笑問客從何處來。");
+		
+	}
+}
